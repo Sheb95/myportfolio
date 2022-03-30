e1a4917be45e85560bef9b56d6c74efca9cd20ea
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-03-30 23:44:46 +0100

    Amplify fails

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+   reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+   exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+   ) {
+      return {
+         "/": { page: "/" },
+         "/about": { page: "/about" },
+         "/contact": { page: "/contact" },
+         "/gallery": { page: "/gallery" },
+      };
+   },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
@@ -7,20 +7,19 @@ export default function Home() {
    return (
       <div className={styles.container}>
          <Head>
-            <title>Sheb's Portfolio Page</title>
+            <title>Sheb Saunders</title>
             <meta
                name="A portfolio page"
                content="Sheb's web development portfoilo"
             />
-            <link rel="icon" href="/favicon.ico" />
          </Head>
-         <h1>Hello, I'm Sheb.</h1>
+         <h1>Hello, I&#39m Sheb.</h1>
          <Link href="/about">
             <a>About me</a>
-         </Link>{" "}
+         </Link>
          <Link href="/contact">
             <a>Contact</a>
-         </Link>{" "}
+         </Link>
          <Link href="/gallery">
             <a>My Photography</a>
          </Link>
