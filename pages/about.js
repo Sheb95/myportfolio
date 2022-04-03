2fb1739b128356cd3d05af3db95f5fbe353bbe43
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-04-03 22:28:15 +0100

    flexbox added. can't resize image via css modules

--- pages/about.js
+++ pages/about.js
@@ -2,28 +2,14 @@ import Navbar from "../components/Navbar/Navbar.js";
 import styles from "../styles/About.module.css";
 export default function About() {
    return (
-      <div className={styles.container}>
+      <div className={styles.wrapper}>
          <Navbar />
-         <main>
-            <h1>About me</h1>
-            <img src="/aboutme.jpg" height="450"></img>
-            <p>
-               I'm baby thundercats PBR&B enamel pin fixie drinking vinegar
-               chambray butcher beard. Godard skateboard plaid jean shorts.
-               Single-origin coffee roof party blue bottle, leggings shoreditch
-               listicle hell of prism humblebrag etsy mixtape keffiyeh locavore
-               forage. Craft beer vinyl thundercats, marfa pickled pop-up +1
-               lyft shabby chic mumblecore. 8-bit four loko jianbing mixtape
-               hella microdosing readymade VHS neutra. Master cleanse iceland
-               letterpress mustache pickled leggings copper mug fingerstache.
-               Church-key helvetica subway tile, ugh bushwick tattooed
-               asymmetrical photo booth selfies pabst wayfarers. Scenester
-               tumeric brooklyn, hoodie cornhole pop-up prism quinoa keytar
-               iPhone. Succulents venmo neutra man braid mixtape la croix
-               raclette gochujang trust fund godard kombucha 3 wolf moon. Yuccie
-               semiotics palo santo church-key neutra prism.
-            </p>
-            <p>
+         <header>
+            <h1 className={styles.heading}>About me</h1>
+         </header>
+         <main className={styles.container}>
+            <img className={styles.image} src="/aboutme.jpg"></img>
+            <p className={styles.text}>
                I'm baby thundercats PBR&B enamel pin fixie drinking vinegar
                chambray butcher beard. Godard skateboard plaid jean shorts.
                Single-origin coffee roof party blue bottle, leggings shoreditch
