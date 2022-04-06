966d4ba764aa19dca417bdb44377438a67305ebe
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-04-06 11:13:40 +0100

    trying to style buttons

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar/Navbar.js";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 import styles from "../styles/Home.module.css";
+import btnstyles from "../components/Button/styles.module.css";
 import { RainbowHighlight } from "../components/RainbowHighlight.js";
 
 export default function Home() {
@@ -18,20 +19,27 @@ export default function Home() {
             />
          </Head>
          <Navbar />
-         <div className={styles.heading}>
-            <RoughNotationGroup show={true}>
-               <RainbowHighlight color={colors[1]}>
-                  <h1>Hello, I am Sheb.</h1>
-               </RainbowHighlight>
-               <RainbowHighlight color={colors[0]}>
-                  <h2>A Fullstack Developer</h2>
-               </RainbowHighlight>
-            </RoughNotationGroup>
-         </div>
+         <div className={styles.wrapper}>
+            <div className={styles.heading}>
+               <RoughNotationGroup show={true}>
+                  <RainbowHighlight color={colors[1]}>
+                     <h1>Hello, I am Sheb.</h1>
+                  </RainbowHighlight>
+                  <RainbowHighlight color={colors[0]}>
+                     <h2>A Fullstack Developer</h2>
+                  </RainbowHighlight>
+               </RoughNotationGroup>
+               <div className={btnstyles.btns}>
+                  <Button className={styles.btn} text="CV" />
+                  <Button
+                     className={btnstyles.btn2}
+                     text="Random Fact About Me"
+                  />
+               </div>
+            </div>
 
-         <img src="/me.png" height="250"></img>
-         <Button text="CV" />
-         <Button text="Random Fact About Me" />
+            <img src="/me.png" height="250"></img>
+         </div>
       </div>
    );
 }
